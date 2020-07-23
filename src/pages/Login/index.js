0b813e9585674f3ea357383f09581d5ce42ae54e
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-23 18:53:50 +0700

    :clown_face: feat: Dummy Login
    cls

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,56 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-import { withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom'
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import styles from './styles'
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import {Copyright} from './../../components'
 
-function Login() {
-  const classes = useStyles();
+const useStyles = styles()
+
+function Login(props) {
+  const classes = useStyles()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [remember, setRemember] = useState(false)
+
+
+  const handleEmailChange = e => setEmail(e.target.value)
+  const handlePasswordChange = e => setPassword(e.target.value)
+  const handleRememberChange = e => setRemember(e.target.checked)
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    localStorage.setItem('token', 'wew')
+    props.history.replace('/dashboard')
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -62,8 +50,10 @@ function Login() {
         <Typography component="h1" variant="h5">
           Log In
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={handleFormSubmit}>
           <TextField
+            value={email}
+            onChange={handleEmailChange}
             variant="outlined"
             margin="normal"
             required
@@ -75,6 +65,8 @@ function Login() {
             autoFocus
           />
           <TextField
+            value={password}
+            onChange={handlePasswordChange}
             variant="outlined"
             margin="normal"
             required
@@ -86,7 +78,7 @@ function Login() {
             autoComplete="current-password"
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
+            control={<Checkbox checked={remember} onChange={handleRememberChange} color="primary" />}
             label="Remember me"
           />
           <Button
@@ -116,7 +108,13 @@ function Login() {
         <Copyright />
       </Box>
     </Container>
-  );
+  )
 }
 
-export default withRouter(Login);
+export default withRouter(Login)
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    replace: PropTypes.func.isRequired,
+  }).isRequired,
+}

6a48aab0d744ca17328e43fbe852210a43a9ff28
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-23 08:27:56 +0700

    :sparkles: feat: Dashboard Page

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+function Dashboard(props) {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default withRouter(Dashboard)

7abf76c3f0b92388618aa9c06001798e9cd24fa5
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-23 08:28:29 +0700

    :wrench: config(route): add basic route

---
+++ src/routes/routes.js
@@ -0,0 +1,31 @@
+import {lazy} from 'react';
+
+import {Lazyload} from './../HOC';
+
+import {Clean as CleanLayout} from './../layouts';
+
+const Login = lazy(() => import('./../pages/Login'));
+const Dashboard = lazy(() => import('./../pages/Dashboard'));
+
+export const routes = [
+  {
+    path: '/',
+    exact: false,
+    title: 'Root',
+    component: CleanLayout,
+    child: [
+      {
+        path: '/login',
+        title: 'Login',
+        exact: true,
+        component: Lazyload(Login)
+      },
+      {
+        path: '/dashboard',
+        title: 'Dashboard',
+        exact: true,
+        component: Lazyload(Dashboard)
+      }
+    ]
+  }
+]

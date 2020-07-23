06aea67f6a405f84537c6b794312804f881b16a0
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-23 08:29:49 +0700

    :art: App with Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import "./App.css";
 
-function App() {
+// Routes
+import {routes} from './routes/routes';
+// Routes
+import { MappedRoute } from './routes/components';
+// Stores
+// Browser history
+const browserHistory = createBrowserHistory();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <MappedRoute history={browserHistory} routes={routes} />
+        </Switch>
+      </BrowserRouter>
   );
-}
+};
 
 export default App;
